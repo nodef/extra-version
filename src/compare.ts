944701d307dc0ec691e132fb7832775071834457
Subhajit Sahu <wolfram77@gmail.com>
2020-05-30 13:32:43 +0530

    :watermelon: cool fruit +compare yo

---
+++ src/compare.ts
@@ -0,0 +1,17 @@
+/**
+ * Compares two versions.
+ * @param x a version
+ * @param y another version
+ * @returns x<y: -1, x=y: 0, x>y: 1
+ */
+function compare(x: IVersion, y: IVersion): number {
+  var c = x.major - y.major;
+  if(c) return c;
+  var c = x.minor - y.minor;
+  if(c) return c;
+  var c = x.patch - y.patch;
+  if(c) return c;
+  var c = x.prerelease.localeCompare(y.prerelease);
+  return c;
+}
+export default compare;
