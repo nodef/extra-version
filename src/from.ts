4ef7b3f4f022c6cdd1ea47fd2e5af97c2193ed12
Subhajit Sahu <wolfram77@gmail.com>
2020-05-30 13:48:38 +0530

    :watermelon: cool fruit +fromString yo

--- src/from.ts
+++ src/from.ts
@@ -7,13 +7,23 @@ const DEFAULT = {
   prerelease: '',
   buildmetadata: ''
 };
+const RVERSION = /^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/;
+
 
 // make this the default yo dawg, rrarf rrarf
 function fromObject(x: IVersion): IVersion {
   return Object.assign({}, DEFAULT, x);
 }
 
-
+// shouldnt this be combined together too
+// what about a combined function
+function fromString(x: string): IVersion {
+  var [,m, n, o, prerelease, buildmetadata] = x.match(RVERSION);
+  var major = parseInt(m, 10);
+  var minor = parseInt(n, 10);
+  var patch = parseInt(o, 10);
+  return {major, minor, patch, prerelease, buildmetadata};
+}
 
 function from(s: string) {
 }
