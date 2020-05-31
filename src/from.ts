57b800751b1d701846fc94d7e820480d4349adf7
Subhajit Sahu <wolfram77@gmail.com>
2020-05-31 14:50:57 +0530

    :watermelon: cool fruit from yo

--- src/from.ts
+++ src/from.ts
@@ -1,27 +1,17 @@
-const REMPTY = /(^|\.)(\.|$)/g;
-const REXTRA = /(^|-\.)0+([1-9]+)/g;
-const RPREFIX = /.*?(\d)/;
-const RNOTPART = /[^\.0-9A-Za-z-]/g;
+import parse from './parse';
+import Version from './Version';
 
 /**
- * Converts version-like to version.
- * @param x a version-like
- * @param inc include empty prerelease, build (false)
+ * Converts value to version.
+ * @param v a value
+ * @returns version, or null
  */
-function from(x: string, inc: boolean=false): string {
-  var x = x.replace(REMPTY, '$10$2').replace(REMPTY, '$10$2');
-  var x = x.replace(REXTRA, '$1$2').replace(RPREFIX, '$1');
-  var i = x.indexOf('+'), bld = i<0? null : x.slice(i+1);
-  var j = x.indexOf('-'), pre = j<0? null : x.slice(j+1, i<0? x.length:i);
-  var bld = bld? bld.replace(RNOTPART, ''):bld;
-  var pre = pre? pre.replace(RNOTPART, ''):pre;
-  var xs = x.slice(0, j<0? x.length:j).split('.');
-  for(var i=0, I=Math.max(xs.length, 3); i<I; i++)
-    xs[i] = isNaN(xs[i] as any)? '0':xs[i];
-  var a = xs.slice(0, 3).join('.');
-  if(pre || inc && pre!=null) a += '-'+pre;
-  if(bld || inc && bld!=null) xs.push(bld);
-  if(xs.length>3) a += '+'+xs.slice(3).join('.');
-  return a;
+function from(v: any): Version {
+  if(typeof v==='number') v = v.toString();
+  if(typeof v==='string') return parse(v, 0, -1)[1];
+  return v && typeof v==='object'? new Version(
+    v.major||0, v.minor||0, v.patch||0,
+    v.prerelease||[], v.buildmetadata||[]
+  ) : null;
 }
 export default from;
--- src/parse.ts
+++ src/parse.ts
@@ -21,11 +21,11 @@ function parseSplit(s: string): string[] {
 }
 
 /**
- * Converts string to version, if possible.
+ * Converts string to version.
  * @param s a string
  * @param i start index (0)
- * @param lvl permissive level (0 => none)
- * @returns found: [end index, version], else: [-1, null]
+ * @param lvl permissive level, 0-4/-1 (0 => none)
+ * @returns [end index, version], or [-1, null]
  */
 function parse(s: string, i: number=0, lvl: number=0): [number, Version] {
   var m = parseRegexp(lvl).exec(s.slice(i));
@@ -36,48 +36,4 @@ function parse(s: string, i: number=0, lvl: number=0): [number, Version] {
   var a = new Version(major, minor, patch, prerelease, buildmetadata);
   return [i+m[0].length, a];
 }
-
-
-/**
- * Converts version-like to version.
- * @param x a version-like
- * @param inc include empty prerelease, build (false)
- */
-function from(x: string, inc: boolean=false): string {
-  var x = x.replace(REMPTY, '$10$2').replace(REMPTY, '$10$2');
-  var x = x.replace(REXTRA, '$1$2').replace(RPREFIX, '$1');
-  var i = x.indexOf('+'), bld = i<0? null : x.slice(i+1);
-  var j = x.indexOf('-'), pre = j<0? null : x.slice(j+1, i<0? x.length:i);
-  var bld = bld? bld.replace(RNOTPART, ''):bld;
-  var pre = pre? pre.replace(RNOTPART, ''):pre;
-  var xs = x.slice(0, j<0? x.length:j).split('.');
-  for(var i=0, I=Math.max(xs.length, 3); i<I; i++)
-    xs[i] = isNaN(xs[i] as any)? '0':xs[i];
-  var a = xs.slice(0, 3).join('.');
-  if(pre || inc && pre!=null) a += '-'+pre;
-  if(bld || inc && bld!=null) xs.push(bld);
-  if(xs.length>3) a += '+'+xs.slice(3).join('.');
-  return a;
-}
-
-
-
-
-const REMPTY = /(^|\.)(\.|$)/g;
-const REXTRA = /(^|-\.)0+([1-9]+)/g;
-const RPREFIX = /.*?(\d)/;
-const RNOTPART = /[^\.0-9A-Za-z-]/g;
-
-/**
- * Gives parts of version.
- * @param x a version
- */
-function parseOld(x: string): IVersion {
-  var x = from(x);
-  var i = x.indexOf('+'), buildmetadata = i<0? null : x.slice(i+1);
-  var j = x.indexOf('-'), prerelease = j<0? null : x.slice(j+1, i<0? x.length:i);
-  var [major, minor, patch] = x.slice(0).split('.').map(parseInt);
-  return {major, minor, patch, prerelease, buildmetadata};
-}
 export default parse;
-
