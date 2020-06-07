5b5766980f292980d0381924f1c41abeea7799a0
Subhajit Sahu <wolfram77@gmail.com>
2020-06-07 09:48:38 +0530

    :watermelon: cool fruit remember property check for is

--- src/is.ts
+++ src/is.ts
@@ -6,5 +6,11 @@ import Version from './Version';
  */
 function is(v: any): boolean {
   return v instanceof Version;
+  // return v && typeof v==='object' &&
+  //   typeof v.major==='number' &&
+  //   typeof v.minor==='number' &&
+  //   typeof v.patch==='number' &&
+  //   v.prerelease==null || Array.isArray(v.prerelease) &&
+  //   v.buildmetadata==null || Array.isArray(v.buildmetadata);
 }
 export default is;
